3de6cc4684b1e6fea3fb5510ab3ffee2fc835f59
j1m5s3 <james.lynch.uml@gmail.com>
2023-12-31 23:01:12 -0600

    New start

---
+++ deprecated/trading-bot-dashboard/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <b-container>
+    <b-row>
+      <UserDashboard/>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import UserDashboard from './components/UserDashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserDashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+@import'~bootstrap-vue/dist/bootstrap-vue.css'
+</style>
---
+++ deprecated/trading-bot-dashboard/src/utils/apis/CoinGeckoApi.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const base_url = process.env.VUE_APP_COINGECKO_API_URL;
+
+export const getCoinGeckoCoinList = async () => {
+  
+
+  let url = base_url + '/coins/list';
+  let cg_res;
+
+  console.log('Making request to: ' + url)
+  cg_res = await axios.get(url, {}).then(response => {
+    if (response.status != 200) {
+      throw new Error('Error fetching coin list');
+    }
+    return response.data;
+  }).catch(error => {
+    console.error('ERROR: ', error);
+  });
+
+  console.log('Got response from: ' + url);
+  console.log(cg_res);
+
+  return cg_res;
+}
+
+export const getCoinGeckoOHLCData = async (coin_id, currency, days) => {
+  let url = base_url + '/coins/' + coin_id + '/ohlc';
+  let cg_res;
+  let params = {
+    vs_currency: currency,
+    days: days
+  };
+
+
+  console.log('Making request to: ' + url)
+  cg_res = await axios.get(url,{}, {params: params}).then(response => {
+    if (response.status != 200) {
+      throw new Error('Error fetching coin list');
+    }
+    return response.data;
+  }).catch(error => {
+    console.error('ERROR: ', error);
+  });
+
+  console.log('Got response from: ' + url);
+  return cg_res;
+}
