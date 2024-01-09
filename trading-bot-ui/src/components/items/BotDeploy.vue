9064e6d3991013618debee26c26d5f66a4944368
j1m5s3 <james.lynch.uml@gmail.com>
2024-01-09 16:33:47 -0600

    Updated deploy config form...

--- trading-bot-ui/src/components/items/BotDeploy.vue
+++ trading-bot-ui/src/components/items/BotDeploy.vue
@@ -3,67 +3,99 @@
         <form @submit.prevent="submitForm">
             <b-row class="mb-3 mx-3">
                 <label for="coinbase_api_key">Coinbase API Key:</label>
-                <input 
-                type="text" 
-                id="coinbase_api_key" 
-                v-model="formData.coinbase_api_key" 
-                placeholder="Enter Coinbase API Key"
-                required />
+                <input type="text" id="coinbase_api_key" v-model="formData.coinbase_api_key"
+                    placeholder="Enter Coinbase API Key" required />
             </b-row>
 
             <b-row class="mb-3 mx-3">
                 <label for="coinbase_api_secret">Coinbase API Secret:</label>
-                <input 
-                type="text" 
-                id="coinbase_api_secret" 
-                v-model="formData.coinbase_api_secret"
-                placeholder="Enter Coinbase API Secret" 
-                required />
+                <input type="text" id="coinbase_api_secret" v-model="formData.coinbase_api_secret"
+                    placeholder="Enter Coinbase API Secret" required />
             </b-row>
 
             <b-row class="mb-3 mx-3">
-                <label for="coin_id">Trade:</label>
-                <select id="coin_id" v-model="formData.coin_id" required>
+                <label for="discord_webhook">Discord Webhook:</label>
+                <input type="text" id="discord_webhook" v-model="formData.discord_webhook"
+                    placeholder="Enter Discord Webhook URL" required />
+            </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="fixed_order_size">Fixed Order Size:</label>
+                <input type="number" id="fixed_order_size" v-model="formData.fixed_order_size"
+                    placeholder="Enter Fixed Order Size" required />
+            </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="buy_order_cash">Buy Order Cash:</label>
+                <input type="number" id="buy_order_cash" v-model="formData.buy_order_cash"
+                    placeholder="Enter Buy Order Cash" required />
+            </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="percent_profit_min">Percent Profit Min:</label>
+                <input type="number" id="percent_profit_min" v-model="formData.percent_profit_min"
+                    placeholder="Enter Percent Profit Min" required />
+            </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="asset_id">Trade:</label>
+                <select id="asset_id" v-model="formData.asset_id" required>
                     <option value="ETH">ETH</option>
                     <option value="XLM">XLM</option>
                     <option value="BTC">BTC</option>
                 </select>
             </b-row>
 
+            <b-row class="mb-3 mx-3">
+                <label for="run_seed">Run Seed:</label>
+                <input type="checkbox" id="run_seed" v-model="formData.run_seed" />
+            </b-row>
 
             <b-row class="mb-3 mx-3">
-                <label for="discord_channel_webhook">Discord Webhook:</label>
-                <input 
-                type="text" 
-                id="discord_channel_webhook" 
-                v-model="formData.discord_channel_webhook" 
-                placeholder="Enter Discord Webhook URL"
-                required />
+                <label for="buy_only">Buy Only:</label>
+                <input type="checkbox" id="buy_only" v-model="formData.buy_only" />
             </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="sell_only">Sell Only:</label>
+                <input type="checkbox" id="sell_only" v-model="formData.sell_only" />
+            </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="accumulate_asset">Accumulate Asset:</label>
+                <input type="checkbox" id="accumulate_asset" v-model="formData.accumulate_asset" />
+            </b-row>
+
+            <b-row class="mb-3 mx-3">
+                <label for="accumulate_cash">Accumulate Cash:</label>
+                <input type="checkbox" id="accumulate_cash" v-model="formData.accumulate_cash" />
+            </b-row>
+
 
             <button type="submit">Deploy Bot</button>
         </form>
     </div>
 </template>
 <style scoped>
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin: auto;
-        width: 75%;
-        padding: 1rem;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-    }
-    form label {
-        font-weight: bold; 
-        display: block; 
-        margin-bottom: 5px;
-        margin-top: 10px;
-        margin-right: 15px;
-    }
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+    width: 75%;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+form label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 5px;
+    margin-top: 10px;
+    margin-right: 15px;
+}
 </style>
 <script>
 export default {
@@ -72,8 +104,16 @@ export default {
             formData: {
                 coinbase_api_key: '',
                 coinbase_api_secret: '',
-                coin_id: '',
-                discord_channel_webhook: '',
+                asset_id: '',
+                discord_webhook: '',
+                fixed_order_size: 0.0,
+                buy_order_cash: 0.0,
+                run_seed: true,
+                percent_profit_min: 2.0,
+                buy_only: false,
+                sell_only: false,
+                accumulate_asset: false,
+                accumulate_cash: true,
             },
         };
     },
@@ -94,7 +134,16 @@ export default {
             this.formData = {
                 coinbase_api_key: '',
                 coinbase_api_secret: '',
-                discord_channel_webhook: ''
+                asset_id: '',
+                discord_webhook: '',
+                fixed_order_size: 0.0,
+                buy_order_cash: 0.0,
+                run_seed: true,
+                percent_profit_min: 2.0,
+                buy_only: false,
+                sell_only: false,
+                accumulate_asset: false,
+                accumulate_cash: true,
             };
         },
     },
